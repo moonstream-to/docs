74608efcb4f7275c9446ea43fff616b26ef7b51d
Neeraj Kashyap <nkashy1@gmail.com>
2022-09-20 05:09:47 -0700

    Added "moonstream" theme

--- generators/inspector_facet.py
+++ generators/inspector_facet.py
@@ -14,7 +14,12 @@ if not os.path.isfile(INSPECTOR_FACET_README_PATH):
     print("No further actions necessary.")
     sys.exit(0)
 
-with open(INSPECTOR_FACET_README_PATH, "r") as ifp, open(INSPECTOR_FACET_DOCS_PATH, "w") as ofp:
+existing_content = ""
+if os.path.isfile(INSPECTOR_FACET_DOCS_PATH):
+    with open(INSPECTOR_FACET_DOCS_PATH, "r") as ifp:
+        existing_content = ifp.read()
+
+with open(INSPECTOR_FACET_README_PATH, "r") as ifp:
     docs_content = ifp.read()
 
     tags_content = """---
@@ -29,7 +34,13 @@ tags:
 
 """
 
-    ofp.write(tags_content)
-    ofp.write(docs_content)
+    # Enforce idempotence of generated file to play nice with "mkdocs serve" live reload.
+    content = f"{tags_content}{docs_content}"
+    if content == existing_content:
+        print(f"No changes to write to {INSPECTOR_FACET_DOCS_PATH}.")
+        sys.exit(0)
+
+    with open(INSPECTOR_FACET_DOCS_PATH, "w") as ofp:
+        ofp.write(content)
 
 print(f"Inspector Facet documentation generated from {INSPECTOR_FACET_README_PATH} and written to {INSPECTOR_FACET_DOCS_PATH}.")
